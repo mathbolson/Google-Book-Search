673144c9ed9795cab544d5801e780f9ba3f68d95
Matheus Bolson <mathbolson@hotmail.com>
2020-11-30 22:51:00 -0800

    Created React components, pages and worked with the api, routes and controllers

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="text-center myText">Google Books Search</h1>
+                <br></br>
+                <h2 className="text-center myText">Search and Save your favorite Books!</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
